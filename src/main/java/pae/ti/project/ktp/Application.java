31e8a85bab79f255250b245ebb4153a699a9210c
Muhammad <muhammadfajarisa@gmail.com>
2022-04-08 14:17:36 +0700

    first commit

---
+++ src/main/java/pae/ti/project/ktp/Application.java
@@ -0,0 +1,13 @@
+package pae.ti.project.ktp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/pae/ti/project/ktp/DataController.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pae.ti.project.ktp;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author fajar
+ */
+@Controller
+public class DataController {
+    
+    DataJpaController datactrl = new DataJpaController();
+    List<Data> newdata = new ArrayList<>();
+    
+    @RequestMapping("/main")
+    public String getMain(){
+        return "menu";
+    }
+    
+    
+    @RequestMapping("/data")
+    //@ResponseBody 
+    public String getDataKTP(Model model){
+        int record = datactrl.getDataCount();
+        String result="";
+        try{
+            newdata = datactrl.findDataEntities().subList(0, record);
+        }
+        catch(Exception e){result=e.getMessage();}
+        model.addAttribute("goData", newdata);
+        model.addAttribute("record", record);
+        return "database";
+        
+    }
+    
+    @RequestMapping("/edit")
+    public String doEdit(){
+        return "editktp";
+    }
+}
